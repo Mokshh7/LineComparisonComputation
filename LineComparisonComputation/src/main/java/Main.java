bce19d5639ad4e4d1aa6039d38c9616f18aedd44
Moksh Jassal <mokshjassal11@gmail.com>
2026-05-14 10:51:18 +0530

    Implimented use case 1

--- LineComparisonComputation/src/main/java/Main.java
+++ LineComparisonComputation/src/main/java/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     static void main(String[] args) {
-        System.out.println("welcome to the line comparison computation ");
-    }
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 4;
+        int y2 = 5;
+
+        double length = Math.sqrt(
+                Math.pow((x2 - x1), 2) +
+                        Math.pow((y2 - y1), 2)
+        );
+
+        System.out.println("Length of line is: " + length);    }
 }
