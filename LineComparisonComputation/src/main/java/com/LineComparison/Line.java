709ad6da2023e42810d8bfd488668eee12f60546
Moksh Jassal <mokshjassal11@gmail.com>
2026-05-15 12:32:33 +0530

    Implemented UC3

--- LineComparisonComputation/src/main/java/com/LineComparison/Line.java
+++ LineComparisonComputation/src/main/java/com/LineComparison/Line.java
@@ -1,13 +1,14 @@
 package com.LineComparison;
 
 public class Line {
+
     int x1;
     int y1;
     int x2;
     int y2;
 
-
     public Line(int x1, int y1, int x2, int y2) {
+
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
--- LineComparisonComputation/src/main/java/com/LineComparison/Main.java
+++ LineComparisonComputation/src/main/java/com/LineComparison/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Line line1 = new Line(1, 2, 4, 6);
-        Line line2 = new Line(2, 3, 5, 7);
+        Line line2 = new Line(2, 3, 10, 12);
 
         double length1 = line1.calculateLength();
         double length2 = line2.calculateLength();
@@ -13,10 +13,14 @@ public class Main {
         System.out.println("Length of Line 1: " + length1);
         System.out.println("Length of Line 2: " + length2);
 
-        if (length1 == length2) {
+        int result = Double.compare(length1, length2);
+
+        if (result == 0) {
             System.out.println("Both lines are equal");
+        } else if (result > 0) {
+            System.out.println("Line 1 is greater than Line 2");
         } else {
-            System.out.println("Both lines are not equal");
+            System.out.println("Line 1 is less than Line 2");
         }
     }
 }
