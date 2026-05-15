10e02bb150e82933bbc50f41085419b3bd61e2eb
Moksh Jassal <mokshjassal11@gmail.com>
2026-05-15 11:15:13 +0530

    Implemented UC2 for comparing line equality

--- LineComparisonComputation/src/main/java/com/LineComparison/Main.java
+++ LineComparisonComputation/src/main/java/com/LineComparison/Main.java
@@ -5,9 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         Line line1 = new Line(1, 2, 4, 6);
+        Line line2 = new Line(2, 3, 5, 7);
 
-        double length = line1.calculateLength();
+        double length1 = line1.calculateLength();
+        double length2 = line2.calculateLength();
 
-        System.out.println("Length of line is: " + length);
+        System.out.println("Length of Line 1: " + length1);
+        System.out.println("Length of Line 2: " + length2);
+
+        if (length1 == length2) {
+            System.out.println("Both lines are equal");
+        } else {
+            System.out.println("Both lines are not equal");
+        }
     }
 }
